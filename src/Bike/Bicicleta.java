ff838a78175687463015f5d1e0035e8fbc28cc4e
JoseTovarC <51676274+JoseTovarC@users.noreply.github.com>
2019-06-19 20:51:39 -0500

    Agregue el constructor a Bicicleta y sus atributos

--- src/Bike/Bicicleta.java
+++ src/Bike/Bicicleta.java
@@ -1,5 +1,19 @@
 package Bike;
 
 public class Bicicleta {
-
+	
+	private int id;
+	private boolean danio = false;
+	private Usuario usuario;
+	private Distribuidor distribuidor;
+	private Estacion estacion;
+	public Bicicleta(int id, boolean danio, Usuario usuario, Distribuidor distribuidor, Estacion estacion) {
+		super();
+		this.id = id;
+		this.danio = danio;
+		this.usuario = usuario;
+		this.distribuidor = distribuidor;
+		this.estacion = estacion;
+	}
+	
 }
