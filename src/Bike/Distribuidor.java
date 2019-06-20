3ddd3ec232ddfa65c530abb8f30aa375ac799c62
Freddy7i <47574684+Freddy7i@users.noreply.github.com>
2019-06-20 16:50:29 -0500

    Add files via upload

---
+++ src/Bike/Distribuidor.java
@@ -0,0 +1,58 @@
+package Bike;
+
+import java.util.ArrayList;
+
+import User.Usuario;
+
+public class Distribuidor {
+	private String nombre;
+	private String id;
+	private int numero_bicis;
+	private ArrayList<Bicicleta> bicicletas = new ArrayList<>();
+	
+	public Distribuidor(String nombre, String id, int numero_bicis){
+		this.nombre = nombre;
+		this.id = id;
+		this.numero_bicis = numero_bicis;
+	}
+	public void finalize() {
+		
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getNumero_bicis() {
+		return numero_bicis;
+	}
+	public void setNumero_bicis(int numero_bicis) {
+		this.numero_bicis = numero_bicis;
+	}
+	
+	public ArrayList<Bicicleta> getBicicletas() {
+		return bicicletas;
+	}
+	public void setBicicletas(ArrayList<Bicicleta> bicicletas) {
+		this.bicicletas = bicicletas;
+	}
+	
+	public void transportar(int id, int q) {
+		
+	}
+	public void arreglar(String id) {
+		
+	}
+	/*public Bicicleta crearB(int q) {
+		
+	}*/
+	
+}
