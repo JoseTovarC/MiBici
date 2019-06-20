8c55d7543f513798bf81376c09ac94f691a18aa5
Luisfemocha <50776653+Luisfemocha@users.noreply.github.com>
2019-06-20 18:15:18 -0500

    Update Persona.java

--- src/User/Persona.java
+++ src/User/Persona.java
@@ -6,5 +6,12 @@ public abstract class Persona {
 	private long doc_id;
 	private String genero;
 	private long clave;
+        public Persona(String nombre, byte edad, long id, String genero, long clave){
+            this.nombre=nombre;
+            this.edad=edad;
+            doc_id=id;
+            this.genero=genero;
+            this.clave=clave;
+        }
 	public abstract void finalize(); //Abstract, obliga a Usuario y moderador definir destructor
 }
